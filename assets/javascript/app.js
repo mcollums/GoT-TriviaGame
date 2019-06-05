ddc6f643e66e6b7ecf5a72c0fcc876082d2bf3c2
mcollums <collumsmichelle@gmail.com>
2019-06-05 17:41:19 -0500

    Start button shows the quiz html
    Now, when the user presses start, the question, timer and choices pop up.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,17 @@
 //Start button appears when page loads.
 //When user click on start button, it disappears and ...
-//The Instructions come up and an OK button
-//When user clicks ok button, instructions and button disappears. Then ...
 //The timer starts, the first question displays, four choices are displayed
     //User presses a button
         //If they choose the right answer...
             //The score is increased by one.
-            //The next question is displayed with new options
+            //Show you're right! with the appropriate gif and blurb
+            //Show next question button that goes to next question after clicked
         //If they choose the wrong answer...
+            //Show you're wrong with the appropriate gif and correct answer
             //The next question is displayed with new options
+            //Show next question button that goes to next question after clicked
+
+
     //At the end, the score is displayed like 7/10 or 4/10
         //Theres a reset button that...
             //Resets the score and displays the first question again
@@ -22,5 +25,21 @@ $(document).ready(function () {
     $(".padding-row").hide();
     $("#choice1-row").hide();
     $("#choice2-row").hide();
+
+    $("#start-button").click(function () {
+        //Shows all elements for the trivia question
+        //Hides instructions
+        $("#instructions-row").hide();
+        $("#ready-gif-row").hide();
+        $("#timer-row").show()
+        $("#question-row").show();
+        $(".padding-row").show();
+        $("#choice1-row").show();
+        $("#choice2-row").show();
+       });
+
+    //An object full of questions and answers
+
+    //
 
 });
