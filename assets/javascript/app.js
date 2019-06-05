33683a7f59a4e2b5ddb2f43951c9f2ae492a860c
mcollums <collumsmichelle@gmail.com>
2019-06-05 17:11:28 -0500

    Pseudocode. Commenting HTML, setting up for JS
    Added comments to HTML file and got rid of the text placeholders. Pseudocoded JS.

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+//Start button appears when page loads.
+//When user click on start button, it disappears and ...
+//The Instructions come up and an OK button
+//When user clicks ok button, instructions and button disappears. Then ...
+//The timer starts, the first question displays, four choices are displayed
+    //User presses a button
+        //If they choose the right answer...
+            //The score is increased by one.
+            //The next question is displayed with new options
+        //If they choose the wrong answer...
+            //The next question is displayed with new options
+    //At the end, the score is displayed like 7/10 or 4/10
+        //Theres a reset button that...
+            //Resets the score and displays the first question again
+
+
+//Start JS
+$(document).ready(function () {
+
+});
